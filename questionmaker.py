c3f6569e543e8280371bb868298c1da2f6acba1c
Nick Tidwell <nicktidwell52@gmail.com>
2024-03-29 17:38:23 -0400

    Reindex data

--- questionmaker.py
+++ questionmaker.py
@@ -93,6 +93,12 @@ class ImageLabelApp:
         output_dir = "output"
         os.makedirs(output_dir, exist_ok=True)
         output_path = os.path.join(output_dir, "questions.csv")
+        
+        self.df['Image_Index'] = self.df['Image'].str.extract(r'(\d+)').astype(int)
+        self.df.sort_values(by='Image_Index', inplace=True)
+        self.df.reset_index(drop=True, inplace=True)
+        self.df.drop(columns=['Unnamed: 0'], inplace=True)
+        self.df.drop(columns=['Image_Index'], inplace=True)
         self.df.to_csv(output_path, index=False)
         print(f"DataFrame saved to {output_path}")
         
