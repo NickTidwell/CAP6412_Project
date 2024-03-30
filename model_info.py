d9a0e58848a27a5f9096792c17edf60e720c1af5
Nick Tidwell <nicktidwell52@gmail.com>
2024-03-30 12:48:34 -0400

    Add ELO System

---
+++ model_info.py
@@ -0,0 +1,28 @@
+data = [
+    {
+        "image_path": "data/000000000.jpg",
+        "question": "Which model is better?",
+        "BLIP2": "BLIP2 is better because...",
+        "INSTRUCT_BLIP": "INSTRUCT_BLIP is better because...",
+        "LLAVA": "Model 1 is better because...",
+        "GIT": "Model 2 is better because...",
+    },
+    {
+        "image_path": "data/000000001.jpg",
+        "question": "Which Do you like better?",
+        "BLIP2": "BLIP2answer",
+        "INSTRUCT_BLIP": "INSTRUCT_BLIP answer",
+        "LLAVA": "LLAVA answer",
+        "GIT": "GIT answer",
+    },
+    {
+        "image_path": "data/000000002.jpg",
+        "question": "Another Test Questino?",
+        "INSTRUCT_BLIP": "INSTRUCT_BLIP sample answer",
+        "BLIP2": "BLIP2answer sample",
+        "LLAVA": "LLAVA is better because...",
+        "GIT": "GIT is better because...",
+    },
+]
+
+MODEL_LIST = ["BLIP2", "INSTRUCT_BLIP", "LLAVA", "GIT"]
